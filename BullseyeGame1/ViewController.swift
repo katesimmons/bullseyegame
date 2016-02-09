67cd0e679bc33c378141a994dcb24f8438fd3a4e
Kate Simmons <kate.a.simmons@gmail.com>
2016-02-09 15:54:39 -0600

    functional

--- BullseyeGame1/ViewController.swift
+++ BullseyeGame1/ViewController.swift
@@ -11,10 +11,10 @@ import UIKit
 class ViewController: UIViewController {
     
     //store values
-    var currentValue: Int = 0
+    var currentValue: Int = 50
     var targetValue: Int = 0
     var score: Int = 0
-    var round: Int = 0
+    var round: Int = 1
     
     //outlets go here
     @IBOutlet weak var slider: UISlider!
@@ -30,6 +30,8 @@ class ViewController: UIViewController {
         startNewRound()
         updateLabels()
         
+        //custom design for slider goes here
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -42,9 +44,36 @@ class ViewController: UIViewController {
     @IBAction func hitMeButton(sender: AnyObject) {
         //everything that happens on submit
         
+        let difference = abs(targetValue - currentValue)
+        var points = 100 - difference
+        
+        //algorithm for points calculations
+        if difference == 0 {
+            title = "Perfect!"
+            points += 100
+        }
+        else if difference < 5 {
+            title = "You almost had it!"
+            if difference == 1 {
+                points += 50
+            }
+        }
+        else if difference < 10 {
+            title = "Not quite"
+        }
+        else {
+            title = "Not even close!"
+        }
+        
+        
+        //score counter
+        score = score + points
+        //print("points: \(points) and score: \(score)")
+        
+        //respons message
         let message = "The value is \(currentValue)" + "\nThe target is \(targetValue)"
         
-        let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .Alert)
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
         
         let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
         
@@ -64,11 +93,43 @@ class ViewController: UIViewController {
     
     func startNewRound() {
         
+        //round counter
+        round += 1 //same as round = round + 1
+        
+        //generate random number
+        targetValue = 1 + Int(arc4random_uniform(100))
+        
+        //create current value for slider
+        currentValue = 50
+        
+        //slider value update
+        slider.value = Float(currentValue)
+        
     }
     
     func updateLabels() {
         
+        //update labels
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+        
     }
+    
+    @IBAction func startOver(sender: AnyObject) {
+        startNewRound()
+        updateLabels()
+        
+        //core animation
+        let transition = CATransition()
+        transition.type = kCATransitionFade
+        transition.duration = 1
+        transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        
+        //run the animation
+        view.layer.addAnimation(transition, forKey: nil)
+    }
+
 
 }
 
